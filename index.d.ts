23be7c359f7e520eed13e11241622f2c5765e5a6
nashika <info@nashika.net>
2016-04-05 03:42:48 +0900

    delete modules

--- index.d.ts
+++ index.d.ts
@@ -1,119 +1,112 @@
-declare module csv_parse_common {
+interface ICsvParseOpts {
 
-  export interface IParseOpts {
+  /**
+   * Set the field delimiter. One character only, defaults to comma.
+   */
+  delimiter?:string;
 
-    /**
-     * Set the field delimiter. One character only, defaults to comma.
-     */
-    delimiter?:string;
+  /**
+   * String used to delimit record rows or a special value;
+   * special constants are 'auto', 'unix', 'mac', 'windows', 'unicode';
+   * defaults to 'auto' (discovered in source or 'unix' if no source is specified).
+   */
+  rowDelimiter?:string;
 
-    /**
-     * String used to delimit record rows or a special value;
-     * special constants are 'auto', 'unix', 'mac', 'windows', 'unicode';
-     * defaults to 'auto' (discovered in source or 'unix' if no source is specified).
-     */
-    rowDelimiter?:string;
+  /**
+   * Optionnal character surrounding a field, one character only, defaults to double quotes.
+   */
+  quote?:string;
 
-    /**
-     * Optionnal character surrounding a field, one character only, defaults to double quotes.
-     */
-    quote?:string;
+  /**
+   * Set the escape character, one character only, defaults to double quotes.
+   */
+  escape?:string;
 
-    /**
-     * Set the escape character, one character only, defaults to double quotes.
-     */
-    escape?:string;
+  /**
+   * List of fields as an array,
+   * a user defined callback accepting the first line and returning the column names or true
+   * if autodiscovered in the first CSV line,
+   * default to null,
+   * affect the result data set in the sense that records will be objects instead of arrays.
+   */
+  columns?:Array<string>|boolean|Function;
 
-    /**
-     * List of fields as an array,
-     * a user defined callback accepting the first line and returning the column names or true
-     * if autodiscovered in the first CSV line,
-     * default to null,
-     * affect the result data set in the sense that records will be objects instead of arrays.
-     */
-    columns?:Array<string>|boolean|Function;
+  /**
+   * Treat all the characters after this one as a comment, default to '' (disabled).
+   */
+  comment?:string;
 
-    /**
-     * Treat all the characters after this one as a comment, default to '' (disabled).
-     */
-    comment?:string;
+  /**
+   * Name of header-record title to name objects by.
+   */
+  objname?:string;
 
-    /**
-     * Name of header-record title to name objects by.
-     */
-    objname?:string;
+  /**
+   * Preserve quotes inside unquoted field.
+   */
+  relax?:boolean;
 
-    /**
-     * Preserve quotes inside unquoted field.
-     */
-    relax?:boolean;
+  /**
+   * Dont generate empty values for empty lines.
+   */
+  skip_empty_lines?:boolean;
 
-    /**
-     * Dont generate empty values for empty lines.
-     */
-    skip_empty_lines?:boolean;
+  /**
+   * If true, ignore whitespace immediately around the delimiter, defaults to false. Does not remove whitespace in a quoted field.
+   */
+  trim?:boolean;
 
-    /**
-     * If true, ignore whitespace immediately around the delimiter, defaults to false. Does not remove whitespace in a quoted field.
-     */
-    trim?:boolean;
+  /**
+   * If true, ignore whitespace immediately following the delimiter (i.e. left-trim all fields), defaults to false. Does not remove whitespace in a quoted field.
+   */
+  ltrim?:boolean;
 
-    /**
-     * If true, ignore whitespace immediately following the delimiter (i.e. left-trim all fields), defaults to false. Does not remove whitespace in a quoted field.
-     */
-    ltrim?:boolean;
+  /**
+   * If true, ignore whitespace immediately preceding the delimiter (i.e. right-trim all fields), defaults to false. Does not remove whitespace in a quoted field.
+   */
+  rtrim?:boolean;
 
-    /**
-     * If true, ignore whitespace immediately preceding the delimiter (i.e. right-trim all fields), defaults to false. Does not remove whitespace in a quoted field.
-     */
-    rtrim?:boolean;
+  /**
+   * If true, the parser will attempt to convert read data types to native types.
+   */
+  auto_parse?:boolean;
 
-    /**
-     * If true, the parser will attempt to convert read data types to native types.
-     */
-    auto_parse?:boolean;
-
-    /**
-     * If true, the parser will attempt to convert read data types to dates. It requires the "auto_parse" option.
-     */
-    auto_parse_date?:boolean;
-
-  }
-
-  type ParseResultType = Array<Array<any>>|Array<{[key:string]:any}>;
-  
-  type ParseCallbackType = (err:Error, output:ParseResultType)=>void;
-
-  export interface Parser extends NodeJS.ReadWriteStream {
-
-    // Parser stream takes array of strings
-    write(data:string):boolean;
-
-    // repeat declarations from NodeJS.WritableStream to avoid compile error
-    write(buffer:Buffer, cb?:Function):boolean;
-    write(str:string, cb?:Function):boolean;
-    write(str:string, encoding?:string, cb?:Function):boolean;
-  }
+  /**
+   * If true, the parser will attempt to convert read data types to dates. It requires the "auto_parse" option.
+   */
+  auto_parse_date?:boolean;
 
 }
 
-declare module "csv-parse" {
+type CsvParseResultType = Array<Array<any>>|Array<{[key:string]:any}>;
 
-  namespace csv_parse {
-    function parse(data:string, options:csv_parse_common.IParseOpts, callback:csv_parse_common.ParseCallbackType):void;
-    function parse(data:string, callback:csv_parse_common.ParseCallbackType):void;
-    function parse(options?:csv_parse_common.IParseOpts):csv_parse_common.Parser;
-    function parse(dataOrOptions:string|csv_parse_common.IParseOpts, optionsOrCallback?:csv_parse_common.IParseOpts|csv_parse_common.ParseCallbackType, callback?:csv_parse_common.ParseCallbackType):void|csv_parse_common.Parser;
-  }
+type CsvParseCallbackType = (err:Error, output:CsvParseResultType)=>void;
 
-  export = csv_parse.parse;
+interface CsvParser extends NodeJS.ReadWriteStream{
 
+  // Parser stream takes array of strings
+  write(data:string):boolean;
+
+  // repeat declarations from NodeJS.WritableStream to avoid compile error
+  write(buffer:any, cb?:Function):boolean;
+  write(str:string, cb?:Function):boolean;
+  write(str:string, encoding?:string, cb?:Function):boolean;
 }
+
+declare function parse(data:string, options:ICsvParseOpts, callback:CsvParseCallbackType):void;
+declare function parse(data:string, callback:CsvParseCallbackType):void;
+declare function parse(options?:ICsvParseOpts):CsvParser;
+declare function parse(dataOrOptions:string|ICsvParseOpts, optionsOrCallback?:ICsvParseOpts|CsvParseCallbackType, callback?:CsvParseCallbackType):void|CsvParser;
+
+export = parse;
+
+/*
 
 declare module "csv-parse/lib/sync" {
 
-  function parse(data:string, options?:csv_parse_common.IParseOpts):csv_parse_common.ParseResultType;
+  function parse(data:string, options?:ICsvParseOpts):CsvParseResultType;
 
   export = parse;
 
 }
+*/
--- test/test.ts
+++ test/test.ts
@@ -1,20 +1,26 @@
 /// <reference path="./main.d.ts" />
 
 import parse = require("csv-parse");
-import parseSync = require("csv-parse/lib/sync")
 
-let input = '#Welcome\n"1","2","3","4"\n"a","b","c","d"';
+test1();
 
-console.log("###Test using the callback API###");
-console.log(`#Input1#`);
-console.log(input);
-parse(input, {comment: '#'}, (err, output1:Array<Array<string>>) => {
-  console.log(`#Output1#`);
-  console.log(output1);
+function test1() {
+  let input = '#Welcome\n"1","2","3","4"\n"a","b","c","d"';
+  console.log("###Test using the callback API###");
+  console.log(`#Input1#`);
+  console.log(input);
+  parse(input, {comment: '#'}, (err, output1:Array<Array<string>>) => {
+    console.log(`#Output1#`);
+    console.log(output1);
+    test2();
+  });
+}
 
+function test2() {
   console.log("###Test using the stream API###");
   let output2:Array<any> = [];
-  let parser:csv_parse_common.Parser = parse({delimiter: ':'});
+  let parser = parse({delimiter: ':'});
+
   parser.on('readable', () => {
     let record:any;
     while (record = parser.read()) {
@@ -27,16 +33,7 @@ parse(input, {comment: '#'}, (err, output1:Array<Array<string>>) => {
   parser.on('finish', () => {
     console.log(`#Output2#`);
     console.log(output2);
-
-    console.log("###Test Using the synchronous API###");
-    let input3 = '"key_1","key_2"\n"value 1","value 2"';
-    console.log(`#Input3#`);
-    console.log(input3);
-    let records:any = parseSync(input3, {columns: true});
-    console.log(`#Output3#`);
-    console.log(records);
-
-    console.log("Finished!");
+    test3();
   });
   let input2:Array<string> = [
     "root:x:0:0:root:/root:/bin/bash\n",
@@ -48,4 +45,16 @@ parse(input, {comment: '#'}, (err, output1:Array<Array<string>>) => {
   parser.write(input2[0]);
   parser.write(input2[1]);
   parser.end();
-});
+}
+
+function test3() {
+/*  console.log("###Test Using the synchronous API###");
+  let input3 = '"key_1","key_2"\n"value 1","value 2"';
+  console.log(`#Input3#`);
+  console.log(input3);
+  let records:any = parseSync(input3, {columns: true});
+  console.log(`#Output3#`);
+  console.log(records);
+
+  console.log("Finished!");*/
+}
